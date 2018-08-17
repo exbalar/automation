543bdbf635d773debfca068e4b19c9d2c7e326b3
Bala Subramanian Ramar <ramarbalasubramanian@gmail.com>
2018-08-17 22:56:58 +0530

    automation

---
+++ src/main/java/com/hackathon/test/FeatureOneTest.java
@@ -0,0 +1,49 @@
+package com.hackathon.test;
+
+public class FeatureOneTest {
+
+	public FeatureOneTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void testOne() {
+		System.out.println("BUC_10B_C: Test One");
+	}
+
+	public void testTwo() {
+		System.out.println("HW12345: Test Two");
+	}
+
+	public void testThree() {
+		System.out.println("Feature1: Test three");
+	}
+
+	public void testFour() {
+		System.out.println("Feaure1: Test four");
+	}
+
+	public void testFive() {
+		System.out.println("Feature1: Test five");
+	}
+
+	public void testSix() {
+		System.out.println("Feature1: Test six");
+	}
+
+	public void testSeven() {
+		System.out.println("Feature1: Test Seven");
+	}
+
+	public void testEight() {
+		System.out.println("Feature1: Test eight");
+	}
+
+	public void testNine() {
+		System.out.println("Feature1: Test nine");
+	}
+
+	public void testTen() {
+		System.out.println("Feature1: Test Ten");
+	}
+
+}
---
+++ src/main/java/com/hackathon/test/FeatureTwoTest.java
@@ -0,0 +1,49 @@
+package com.hackathon.test;
+
+public class FeatureTwoTest {
+
+	public FeatureTwoTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void testOne() {
+		System.out.println("HW12345: Test One");
+	}
+
+	public void testTwo() {
+		System.out.println("HW12345: Test Two");
+	}
+
+	public void testThree() {
+		System.out.println("HW12345: Test three");
+	}
+
+	public void testFour() {
+		System.out.println("HW12345: Test four");
+	}
+
+	public void testFive() {
+		System.out.println("HW12345: Test five");
+	}
+
+	public void testSix() {
+		System.out.println("HW12345: Test six");
+	}
+
+	public void testSeven() {
+		System.out.println("HW12345: Test Seven");
+	}
+
+	public void testEight() {
+		System.out.println("HW12345: Test eight");
+	}
+
+	public void testNine() {
+		System.out.println("HW12345: Test nine");
+	}
+
+	public void testTen() {
+		System.out.println("HW12345: Test Ten");
+	}
+
+}
